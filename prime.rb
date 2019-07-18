b3199d82f83028c92302d4e261d92778935824bf
Veronica Lopez <nicaa0695@gmail.com>
2019-07-18 20:52:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime(array)
+  range = (2..(number-1)).to_a 
+  check = true 
+  if number < 2 
+   check false 
+  else
+    range.each do |x|
+      if number % x == 0 
+        check false 
+        break
+      end
+    end
+  end 
+    puts check
+end
+    
