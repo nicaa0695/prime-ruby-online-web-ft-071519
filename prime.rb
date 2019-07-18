9a1e8b5442392f3754c0144577aecb28fc8bb0d5
Veronica Lopez <nicaa0695@gmail.com>
2019-07-18 20:54:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,6 @@
 # Add  code here!
 def prime?(number)
-  range = (2..(number-1)).to_a 
-  check = true 
-  if number < 2 
-   check false 
-  else
-    range.each do |x|
-      if number % x == 0 
-        check false 
-        break
-      end
-    end
-  end 
-    puts check
+  check false 
+  count = 2 
+  return check 
 end
-    
